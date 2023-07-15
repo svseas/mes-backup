b7e0f44aecc42b6cdfbd7b845f0a1c1fc675e643
Truong Tang <txt2408@gmail.com>
2023-07-15 15:32:44 +0700

    fix model

--- local-addons/mes-bom/models/mes_bom.py
+++ local-addons/mes-bom/models/mes_bom.py
@@ -2,27 +2,38 @@
 
 from odoo import models, fields, api
 
+
 class TechSequence(models.Model):
     _name = 'tech.sequence'
     _rec_name = 'order'
 
     order = fields.Integer(string='Order')
 
+class TechStage(models.Model):
+    _name = 'tech.stage'
+    _rec_name = 'name'
+
+    name = fields.Char('Stage Name')
+    machine_ids = fields.Char('Machines')
+    description = fields.Char('Description')
+    waste_percent = fields.Float('Waste Percent')
+    sequence = fields.Many2one('tech.sequence', string='Sequence')
 
 class TechProcess(models.Model):
     _name = 'tech.process'
     _rec_name = 'name'
 
     name = fields.Char(string='Process Name')
+    tech_stage_ids = fields.Many2one(string='Technical Stage')
     description = fields.Char(string='Process Description')
     input_description = fields.Char(string='Input Description')
     output_description = fields.Char(string='Output Description')
     image = fields.Image(string='Image')
     sequence = fields.Many2one('tech.sequence', string='Sequence')
 
+
 class Bom(models.Model):
     _inherits = ['mrp.bom']
 
-    tech_process_ids = fields.Many2one('tech.process')
-
-
+    tech_process_ids = fields.Many2one('tech.process', 'Technical Process')
+    time_process = fields.Float('Time Process')
