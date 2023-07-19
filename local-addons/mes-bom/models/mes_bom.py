f01273198d9923255f47517980533dfbccea2a60
svseas <txt2408@gmail.com>
2023-07-19 15:57:22 +0700

    fix tech process

--- local-addons/mes-bom/models/mes_bom.py
+++ local-addons/mes-bom/models/mes_bom.py
@@ -19,7 +19,14 @@ class MrpWorkerGroup(models.Model):
                             help='Capacity is in hours')
     worker_ids = fields.Many2many('res.users',
                                   string="Worker List")
+    bom_id = fields.Many2one('mrp.bom', string='BOM')
+    tech_process_id = fields.Many2one('tech.process', string='Technical Stage')
 
+
+class ProductProducts(models.Model):
+    _inherit = 'product.product'
+
+    tech_process_id = fields.Many2one('tech.process', string='Tech Process')
 
 class TechSequence(models.Model):
     """Technological Sequence to be added"""
@@ -27,8 +34,6 @@ class TechSequence(models.Model):
     _rec_name = 'order'
 
     order = fields.Integer(string='Order')
-
-
 
 
 class TechStage(models.Model):
@@ -57,7 +62,8 @@ class TechProcess(models.Model):
                                      string='Technical Stages')
     description = fields.Char(string='Process Description')
     input_description = fields.Char(string='Input Description')
-    input = fields.Many2one('product.product',
+    input = fields.One2many('product.product',
+                            'tech_process_id',
                             string="Input")
     output_description = fields.Char(string='Output Description')
     output = fields.Many2one('product.product',
@@ -67,6 +73,7 @@ class TechProcess(models.Model):
     ng_percent = fields.Float(string='NG Percent')
     bom_id = fields.Many2one('mrp.bom',
                              string='BOM')
+    worker_group_ids = fields.One2many('worker.group', 'tech_process_id', string='Worker Group')
 
 
 class Bom(models.Model):
@@ -79,4 +86,6 @@ class Bom(models.Model):
     ng_percent = fields.Float(string='NG Percent')
     created_by = fields.Many2one('res.users',
                                  string='Created By')
+    worker_group_ids = fields.One2many('worker.group', 'bom_id', string='Worker Group')
+
 
