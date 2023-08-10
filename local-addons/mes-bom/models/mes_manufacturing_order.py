c1d3ea3c3bec1f2897e1257cd140682aa8879d2d
Truong Tang <txt2408@gmail.com>
2023-08-10 22:04:53 +0700

    fix MO

--- local-addons/mes-bom/models/mes_manufacturing_order.py
+++ local-addons/mes-bom/models/mes_manufacturing_order.py
@@ -21,25 +21,8 @@ class ManufacturingOrder(models.Model):
     document = fields.Binary(string='Document')
     bom_tech_process_ids = fields.Many2many(related='bom.tech_process_ids', string='BOM Tech Processes')
 
-    combined_inputs = fields.Many2many('material.line', compute='_compute_combined_inputs', string='Combined Inputs')
+    combined_inputs = fields.Many2many(related='bom.combined_inputs', string='Combined Inputs')
 
-    def get_combined_inputs(self, bom):
-        # Compute combined inputs for the given BOM
-        # Return the combined inputs as a Many2many recordset
-        combined_inputs = self.combined_inputs
-        # Implement the logic to compute combined inputs based on the BOM
-        return combined_inputs
 
-    @api.depends('bom_tech_process_ids')
-    def _compute_combined_processes(self):
-        for record in self:
-            combined_processes = record.bom_tech_process_ids
-            record.combined_processes = combined_processes
 
-    @api.depends('bom')
-    def _compute_combined_inputs(self):
-        for record in self:
-            if record.bom:
-                combined_inputs = record.bom.tech_process_id.get_combined_inputs(record.bom)
-                record.combined_inputs = combined_inputs
 
