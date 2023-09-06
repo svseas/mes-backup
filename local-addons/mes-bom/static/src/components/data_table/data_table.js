c7427d7c8ec7412bb7c710f38e531c96bacc4af4
Sharko0511 <duyquang511@gmail.com>
2023-09-06 19:46:33 +0700

    fix data table

--- local-addons/mes-bom/static/src/components/data_table/data_table.js
+++ local-addons/mes-bom/static/src/components/data_table/data_table.js
@@ -1,56 +1,27 @@
-/** @odoo-module */
+/**@odoo-module */
+const { Component, onMounted, useState } = owl;
+import { registry } from "@web/core/registry";
+const actionRegistry = registry.category("actions");
+const rpc = require('web.rpc');
+export class DataTable extends Component {
+    setup(){
+        super.setup(...arguments);
+        onMounted(()=>{
+            this.renderTable();
+        })
+    }
 
-import { loadBundle } from "@web/core/assets";
-var AbstractAction = require('web.AbstractAction');
-var core = require('web.core');
-var QWeb = core.qweb;
-var rpc = require('web.rpc');
-const dataArray = [
-  {   level: 1,
-      process_code: 'John',
-      process_name: 30,
-      process_desc: "",
-      output: "",
-      output_image: "",
-      ingredient: "",
-      product_code: "",
-      component_code_1: "",
-      component_code_2: "",
-      material_code: "",
-      material_name: "",
-  },
-    {   level: 1,
-      process_code: 'John',
-      process_name: 30,
-      process_desc: "",
-      output: "",
-      output_image: "",
-      ingredient: "",
-      product_code: "",
-      component_code_1: "",
-      component_code_2: "",
-      material_code: "",
-      material_name: "",
-  },
-];
-
-var DataTable = AbstractAction.extend({
-    template: 'mes-bom.DataTable'
-
-    start: function() {
-        this.set("title", 'DataTable');
-        this.renderTable(); // Assuming you have a renderTable method
-        return this._super();
-    },
-
-    renderTable: function() {
+    renderTable() {
         const self = this;
-        const tableContainer = self.$el.find('#table-container'); // Assuming you have a container with this ID
-        const table = document.createElement('table');
-
-        tableContainer.append(table); // Append the table to the container
-    },
-});
-
-core.action_registry.add('mes-bom.action_data_table_js', DataTable);
-return DataTable;
+        const template = `
+            <div>1</div>
+        `;
+//        self.$('.table-container').append(template)
+        function creatTable(template) {
+        self.$('.table-container').append(template)
+        };
+        console.log("Test============= ")
+    }
+}
+DataTable.template = "mes-bom.DataTable";
+actionRegistry.add("mes-bom.action_data_table_js", DataTable);
