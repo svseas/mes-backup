19afd55a99118bc4f589a4a151e827b7487a00a6
Sharko0511 <duyquang511@gmail.com>
2023-09-08 00:09:42 +0700

    add outpu and material

--- local-addons/mes-bom/static/src/components/data_table/data_table.js
+++ local-addons/mes-bom/static/src/components/data_table/data_table.js
@@ -6,20 +6,25 @@ const rpc = require('web.rpc');
 export class DataTable extends Component {
     setup(){
         super.setup(...arguments);
-        this.dataTable = useState({ data: [] })
+        this.dataTable = useState({ data: [], keys: [], dict: [] })
         onMounted(()=>{
             this.loadData();
         })
+
     }
     loadData(){
         let self = this;
          rpc.query({
             model: 'tech.process',
             method: 'get_table_data',
-            }).then(function(data){
-                self.dataTable.data = data;
-                console.log("Data=============: ", data)
-        });
+         }).then(function(data){
+             self.dataTable.data = data[0];
+             self.dataTable.keys = data[1];
+             self.dataTable.dict = data[2];
+             console.log("Data=============: ", data)
+         }).catch(function(error) {
+            console.error('Error fetching data for ', error);
+         });
     }
 }
 DataTable.template = "mes-bom.DataTable";
