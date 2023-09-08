9e0fd337e7d5b7b594cc8b683df1fee1c42fa2e8
Sharko0511 <duyquang511@gmail.com>
2023-09-08 18:51:51 +0700

    edit data

--- local-addons/mes-bom/models/data_table.py
+++ local-addons/mes-bom/models/data_table.py
@@ -11,18 +11,14 @@ class DataTable(models.Model):
 
         result = []
         for record in data:
-            child_process_names = [child.name for child in record.child_process_ids]
             output_names = [output.output.name for output in record.output if output.output]
-            # This will trigger the compute method
-            record.child_process_inputs
-            child_input_names = [input.name for input in record.child_process_inputs]
+            child_process_ids = [child.id for child in record.child_process_ids]
             record_data = {
                 'level': record.level if hasattr(record, 'level') else '1',
                 'process_level': record.process_level if hasattr(record, 'process_level') else '1.1',
                 'name': record.name,
-                'process_names': ', '.join(child_process_names),
-                'output_names': ', '.join(output_names),
-                'child_input_names': ', '.join(child_input_names),
+                'child_process_ids': child_process_ids,
+                'output_name': ', '.join(output_names),
             }
             result.append(record_data)
 
@@ -31,30 +27,50 @@ class DataTable(models.Model):
             'level': "Level",
             'process_level': "Bước CĐ",
             'name': "Công đoạn",
-            'process_names': "Nội dung công đoạn",
-            'output_names': 'Thành phần đầu ra',
+            'process_name': "Nội dung công đoạn",
+            'output_name': 'Thành phần đầu ra',
             'Hình ảnh đầu ra': '',
             'Quy cách thành phần': '',
             'Mã sản phẩm': '',
             'Mã thành phần cấp 1': '',
             'Mã thành phần cấp 2': '',
             'Mã Nguyên liệu': '',
-            'child_input_names': 'Tên nguyên liệu',
+            'input_names': 'Tên nguyên liệu',
         }
 
         table_dict = {
             'Level': "level",
             'Bước CĐ': "process_level",
             "Công đoạn": 'name',
-            'Nội dung công đoạn': "process_names",
-            'Thành phần đầu ra': 'output_names',
+            'Nội dung công đoạn': "process_name",
+            'Thành phần đầu ra': 'output_name',
             'Hình ảnh đầu ra': '',
             'Quy cách thành phần': '',
             'Mã sản phẩm': '',
             'Mã thành phần cấp 1': '',
             'Mã thành phần cấp 2': '',
             'Mã Nguyên liệu': '',
-            'Tên nguyên liệu': 'child_input_names',
+            'Tên nguyên liệu': 'input_names',
         }
 
         return result, table_keys, table_dict, pure_data.read()
+
+    @api.model
+    def get_child_process(self, ids):
+        results = []
+        for id in ids:
+            data = self.env['tech.process'].search([('id', '=', id)])
+
+            for record in data:
+                output_names = [output.output.name for output in record.output if output.output]
+                input_names = [input.name for input in record.child_process_inputs]
+                record_data = {
+                    'level': record.level if hasattr(record, 'level') else '2',
+                    'process_level': record.process_level if hasattr(record, 'process_level') else '1.2',
+                    'process_name': record.name,
+                    'output_name': ', '.join(output_names),
+                    'input_names': ', '.join(input_names),
+                }
+                results.append(record_data)
+
+        return results
--- local-addons/mes-bom/static/src/components/data_table/data_table.js
+++ local-addons/mes-bom/static/src/components/data_table/data_table.js
@@ -6,7 +6,12 @@ const rpc = require('web.rpc');
 export class DataTable extends Component {
     setup(){
         super.setup(...arguments);
-        this.dataTable = useState({ data: [], keys: [], dict: [] })
+        this.dataTable = useState({ data: [],
+                                    keys: [],
+                                    dict: [],
+                                    child_process: [],
+                                    i: [1],
+                                    })
         onMounted(()=>{
             this.loadData();
         })
@@ -21,6 +26,22 @@ export class DataTable extends Component {
              self.dataTable.keys = data[1];
              self.dataTable.dict = data[2];
              console.log("Data=============: ", data)
+         }).catch(function(error) {
+            console.error('Error fetching data for ', error);
+         });
+    }
+    loadChildProcess(ids){
+         let self = this;
+         let myArguments = ids
+         console.log("arg================ ", myArguments)
+         rpc.query({
+            model: 'tech.process',
+            method: 'get_child_process',
+            args:[myArguments]
+         }).then(function(data){
+             self.dataTable.child_process = data;
+             self.dataTable.i = i++
+             console.log("DataChild=============: ", data,i )
          }).catch(function(error) {
             console.error('Error fetching data for ', error);
          });
